9405d0c56ab1619a3211b3607efbb5ebeeb626e6
mwu82507-boop <mwu82507@gmail.com>
2026-02-07 23:30:59 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        appear: {
+          from: {
+            opacity: 0
+          },
+          to: {
+            opacity: 1
+          }
+        },
+        slide: {
+          from: {
+            transform: "translateY(-100%)"
+          },
+          to: {
+            transform: "translateY(0%)"
+          }
+        } 
+      },
+      animation: {
+        appear: "appear 1s ease-in-out",
+        slide: "slide 750ms ease-in-out",
+
+      },
+    }
+  },
+  plugins: [],
+};
